40e346e17743ae37cc52696552614b60686a44e4
Anastasiia Kurulenko <kurulenkoanastasia@gmail.com>
2025-05-13 21:57:36 +0200

    viewing appointments functionality for doctor added

--- src/main/java/pl/edu/hospital/controller/DoctorController.java
+++ src/main/java/pl/edu/hospital/controller/DoctorController.java
@@ -1,16 +1,27 @@
 package pl.edu.hospital.controller;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+import pl.edu.hospital.dto.AppointmentForDoctorDto;
 import pl.edu.hospital.dto.ConsultationDto;
+import pl.edu.hospital.entity.enums.Status;
 import pl.edu.hospital.entity.enums.WorkingDay;
 import pl.edu.hospital.exception.DoctorNotFoundException;
+import pl.edu.hospital.service.AppointmentService;
 import pl.edu.hospital.service.ConsultationService;
 import pl.edu.hospital.service.DoctorService;
 
+import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -21,10 +32,13 @@ import java.util.stream.Collectors;
 public class DoctorController {
     private final DoctorService doctorService;
     private final ConsultationService consultationService;
+    private final AppointmentService appointmentService;
 
-    public DoctorController(DoctorService doctorService, ConsultationService consultationService) {
+    public DoctorController(DoctorService doctorService, ConsultationService consultationService,
+                            AppointmentService appointmentService) {
         this.doctorService = doctorService;
         this.consultationService = consultationService;
+        this.appointmentService = appointmentService;
     }
 
     @GetMapping("/home")
@@ -59,5 +73,46 @@ public class DoctorController {
         }
 
         return "doctor_pages/doctor_schedule";
+    }
+
+    @PostMapping("/apps")
+    public RedirectView getAppointmentsForDoctorInRange(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                                        LocalDate startDate,
+                                                        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                                        LocalDate endDate,
+                                                        @RequestParam(required = false) Status status,
+                                                        RedirectAttributes redirectAttributes) {
+
+        //using authentication
+        String username = "rgarcia";
+
+        LinkedHashMap<LocalDate, List<AppointmentForDoctorDto>> appointments =
+                appointmentService.getAppointmentsForDoctorFiltered(username, startDate, endDate, status);
+
+        redirectAttributes.addFlashAttribute("dFullName", doctorService.getDoctorFullNameByUsername(username));
+        redirectAttributes.addFlashAttribute("username", username);
+        if (startDate.isAfter(endDate)) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Invalid date range");
+        } else if (appointments.isEmpty()) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Not data found");
+        } else {
+            redirectAttributes.addFlashAttribute("appointments", appointments);
+        }
+
+        return new RedirectView("appointments");
+    }
+
+    @GetMapping("/appointments")
+    public String getAppointmentsForDoctor(Model model) {
+        //using authentication
+        String username = "rgarcia";
+        try {
+            model.addAttribute("dFullName", doctorService.getDoctorFullNameByUsername(username));
+            model.addAttribute("statuses", Status.values());
+        } catch (DoctorNotFoundException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+        }
+
+        return "doctor_pages/doctor_appointments";
     }
 }
